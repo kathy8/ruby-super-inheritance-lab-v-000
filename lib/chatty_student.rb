0620fbcc063789b4b12b7746f1515b004fa3083b
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-07-07 01:40:37 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,6 +3,8 @@
 class ChattyStudent < Student
 
 def hello
+  super
+  puts "Hey there! I'm so excited to learn stuff."
   puts "How are you doing today?
   I'm okay, but I'm kind of tired. Did you watch
   The Walking Dead last night? You didn't?! Oh man,
